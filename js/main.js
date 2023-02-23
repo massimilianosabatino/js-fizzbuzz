e48e4f2a3f5a2b2f984e5203cf764aa507f32cde
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-02-23 17:23:06 +0100

    adds classes to div dynamically

--- js/main.js
+++ js/main.js
@@ -8,17 +8,23 @@ const containerDiv = document.getElementById('container');
 
 //Ciclo per generare numeri
 for (let i = 1; i < 101; i++) {
+    //Creazione variabile div per creare un div html ogni volta che viene eseguito il ciclo
+    const div = document.createElement("div");
     numberCurrent = i;
     if (i % 3 === 0 && i % 5 === 0) {
         numberCurrent = 'fizzbuzz';
+        div.classList.add('fizz', 'buzz');
     } else if (i % 3 === 0) {
         numberCurrent = 'fizz';
+        div.classList.add('fizz');
     } else if (i % 5 === 0) {
         numberCurrent = 'buzz';
+        div.classList.add('buzz');
     }
     console.log(numberCurrent);
 
-    const div = document.createElement("div");
+    //inserimento dentro al div il valore della variabile genrata dal ciclo
     div.append(numberCurrent);
+    //Inserisce tutto dentro un div che fa da container
     containerDiv.append(div)
 }
