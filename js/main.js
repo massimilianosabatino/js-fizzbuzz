872c3355f6a6b927827f0930a9a815f924088929
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-02-23 15:37:19 +0100

    number 1 -100 and fizz + buzz - but duplicated

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//Ciclo per generare numeri
+for (let i = 1; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+        console.log('fizz');
+    } else if (i % 5 === 0) {
+        console.log('buzz');
+    console.log(i);
+    }
+    console.log(i);
+}
